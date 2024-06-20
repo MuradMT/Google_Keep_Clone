fa73d84729a66e50683b3d494b22623d0f8cd139
MuradMT <mammadzade03@gmail.com>
2024-06-20 22:53:40 +0400

    Added gitignore.

--- src/Backend/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Backend/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ca0da824495d295fd3fe5aa6cb5b28e42c7f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23969c593cb7113ed30ebcd782543ec361153225")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
